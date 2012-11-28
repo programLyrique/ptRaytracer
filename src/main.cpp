d89d1708f4640b0025510042b12e25a614c310c3
Thibaud <thibaud@thibaud.(none)>
2012-11-28 13:52:54 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <unistd.h>
-
 #include "screen.hpp"
+#include "scene.h"
+
+
 
 int main(void)
 {
 	rt::screen s(640,480);
 
 	// here the code to draw the pixels...
+	Scene scene();
+	
+	
 
 	while(s.update()) {
-		// wait for an input from user
+		scene.render(&s);
 	}
 
 	return 0;
