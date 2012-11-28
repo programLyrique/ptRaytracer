61261e9eae611c38796a89d9fe6a87100c985792
Thibaud <thibaud@thibaud.(none)>
2012-11-28 13:18:14 +0100

    Deplacement du rendu

--- src/camera.cpp
+++ src/camera.cpp
@@ -10,28 +10,7 @@ namespace rt
 
     void Camera::generate(int height, int width)
     {
-        rt::vector _up = up.norm();
-        rt::vector vcenter = rt::vector(centre.getX() - eye.getX(), centre.getY() - eye.getY(), centre.getZ() - eye.getZ())
-        rt::vector right = vcenter.norm()^_up;
-
-        image im(width, height);
-
-        for(int i = 0; i < width; ++i)
-        {
-            for(int j = 0; j < height; ++j)
-            {
-                bool inter = false;
-                for(int k = 0; k < parent.getObjets.size(); k++)
-                {
-                    if(parent.getObjets()[k].intersect(eye, vcenter + (i - width / 2) * right + (j - height / 2) * _up))
-                        inter = true;
-                }
-                if(inter)
-                    im.set_pixel(i, j, color::BLACK);
-                else
-                    im.set_pixel(i, j, color::WHITE);
-            }
-        }
+        
     }
 
 }
--- src/scene.cpp
+++ src/scene.cpp
@@ -7,9 +7,28 @@ namespace rt
 
     }
 
-    void Scene::render(const screen& s)
+    void Scene::render(screen& s)
     {
+	rt::vector _up = up.norm();
+        rt::vector vcenter = rt::vector(centre.getX() - eye.getX(), centre.getY() - eye.getY(), centre.getZ() - eye.getZ())
+        rt::vector right = vcenter.norm()^_up;
 
+        for(int i = 0; i < s.width(); ++i)
+        {
+            for(int j = 0; j < s.height(); ++j)
+            {
+                bool inter = false;
+                for(int k = 0; k < objets.size(); k++)
+                {
+                    if(objets[k].intersect(eye, vcenter + (i - width / 2) * right + (j - height / 2) * _up))
+                        inter = true;
+                }
+                if(inter)
+                    s.set_pixel(i, j, color::BLACK);
+                else
+                    s.set_pixel(i, j, color::WHITE);
+            }
+        }
     }
 
 
