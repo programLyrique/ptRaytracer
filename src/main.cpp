2affc73ab6cd196d3daaa0dbd326e20b610408c0
programLyrique <program@lyrique.org>
2012-11-29 18:34:20 +0100

    To play with the renderer and test it : adding a red sphere.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main(void)
 	rt::Scene scene;
 	scene.setCamera(new rt::Camera(rt::Position(0, 0, 0), rt::Position(0, 5, 0), rt::vector(0, 0, 1)));
 	scene.addMesh(new rt::Sphere(rt::Position(0, 15, 0), 10, rt::Texture(rt::color::BLUE)));
+	scene.addMesh(new rt::Sphere(rt::Position(-50, 20, -3), 20, rt::Texture(rt::color::RED)));
 	scene.render(s);
 
 
