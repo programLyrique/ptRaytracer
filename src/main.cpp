b53efc0e17be8ae54aa88e3d21794109a3f649de
Thibaud <thibaud@thibaud.(none)>
2012-12-03 22:25:10 +0100

    main legerement changé

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,8 @@ int main(void)
     // here the code to draw the pixels...
     rt::Scene scene;
     scene.setCamera(new rt::Camera(rt::Position(0, 0, 0), rt::Position(0, 500, 0), rt::vector(0, 0, 1)));
-    //scene.addLight(new rt::PointLight(20., 25., 0., rt::color::WHITE, &scene));
-    //scene.addLight(new rt::PointLight(-20, -25, 0, rt::color::WHITE, &scene));
-    scene.addLight(new rt::PointLight(-20., 0., 0., rt::color::WHITE, &scene));
+    scene.addLight(new rt::PointLight(20., 25., 0., rt::color::WHITE, &scene));
+    scene.addLight(new rt::PointLight(-20, -25, 0, rt::color::WHITE, &scene));
     for(int k = 0 ; k < NB_SPHERES ; k++)
     {
         scene.addMesh(new rt::Sphere(rt::Position(0 * cos(k), 20 + 35* cos(k), 25 * cos(k) ), 3, rt::Texture(rt::color::RED, rt::color::BLUE, 1)));
