8f730e52957b8ce6a674e70eaf499d0d57c2c1bd
programLyrique <program@lyrique.org>
2012-12-04 20:44:04 +0100

    Oversampling debugged.

--- src/scene.cpp
+++ src/scene.cpp
@@ -6,194 +6,202 @@
 
 namespace rt
 {
-    Scene::Scene()
-    {
+Scene::Scene()
+{
 
-    }
+}
 
-    void Scene::renderArea(int x, int y, int width, int height, screen& s, bool oversampling = true)
+void Scene::renderArea(int x, int y, int width, int height, screen& s, bool oversampling = true)
+{
+    vector up = cam->getUp() ;
+    Position centre = cam->getCentre();
+    Position eye = cam->getEye();
+    vector _up = up.unit();
+    vector vcenter = vector(centre.getX() - eye.getX(), centre.getY() - eye.getY(), centre.getZ() - eye.getZ());
+    vector right = vcenter.unit() ^ _up;
+    // Rendering of the rectangle.
+    for(int i = x; i < x + width; ++i)
     {
-        vector up = cam->getUp() ;
-        Position centre = cam->getCentre();
-        Position eye = cam->getEye();
-        vector _up = up.unit();
-        vector vcenter = vector(centre.getX() - eye.getX(), centre.getY() - eye.getY(), centre.getZ() - eye.getZ());
-        vector right = vcenter.unit() ^ _up;
-        // Rendering of the rectangle.
-        for(int i = x; i < x + width; ++i)
+        for(int j = y; j < y + height; ++j)
         {
-		  for(int j = y; j < y + height; ++j)
-		   {
-                double step = 0;
-                double mL1 = 0;
-                double mL2 = 0;
-                double mL3 = 0;
-				// Oversampling x 9
-				if(oversampling)
-				    step = 0.25;
+            double step = 0;
+            double mL1 = 0;
+            double mL2 = 0;
+            double mL3 = 0;
+            // Oversampling x 9
+            if(oversampling)
+                step = 0.25;
 
-					for(double ho=i - step; ho <= i + step ; ho += step)
-						for(double ve=j-step; ve <= j + step ; ve+=step)
-						{
-                          //  std::cout  << ho << " et " << ve << std::endl;
-						  bool inter = false;
-						  rt::Position p;
-						  int o = 0;
-						  vector v = (vcenter + (ho - s.width() / 2.0) * right + (ve - s.height() / 2.0) * _up).unit();
-						  for(unsigned k = 0; k < objets.size(); k++)
-					      {
-							  if(objets[k]->intersect(eye, v))
-							  {
+            for(double ho=i - step; ho <= i + step ; ho += step)
+                for(double ve=j-step; ve <= j + step ; ve+=step)
+                {
+                    if(not oversampling)
+                    {
+                        ho++;//eviter une boucle infinie
+                        ve++;
+                    }
+                    //  std::cout  << ho << " et " << ve << std::endl;
+                    bool inter = false;
+                    rt::Position p;
+                    int o = 0;
+                    vector v = (vcenter + (ho - s.width() / 2.0) * right + (ve - s.height() / 2.0) * _up).unit();
+                    for(unsigned k = 0; k < objets.size(); k++)
+                    {
+                        if(objets[k]->intersect(eye, v))
+                        {
 
-								rt::Position pos = objets[k]->getIntersection(eye, v);
-								if(inter)
-								{
-					                if(eye.distance(pos) < eye.distance(p))
-					                {
-					                   p = pos;
-					                   o = k;
-					                }
+                            rt::Position pos = objets[k]->getIntersection(eye, v);
+                            if(inter)
+                            {
+                                if(eye.distance(pos) < eye.distance(p))
+                                {
+                                    p = pos;
+                                    o = k;
+                                }
 
-								}
-								else
-								{
-									inter = true;
-									p = pos;
-									o = k;
-								}
-					           }
-					      }
+                            }
+                            else
+                            {
+                                inter = true;
+                                p = pos;
+                                o = k;
+                            }
+                        }
+                    }
 
-						  if(inter)
-						  {
-						  	vector tem = (-1 * v).unit();
-						  	double l1 = 0;
-						  	double l2 = 0;
-						  	double l3 = 0;
+                    if(inter)
+                    {
+                        vector tem = (-1 * v).unit();
+                        double l1 = 0;
+                        double l2 = 0;
+                        double l3 = 0;
 
-						  	for(int k = 0; k < lights.size(); ++k)
-						  	{
-						  		l1 += lights[k]->illuminateR(p, (Sphere*) objets[o], tem);
-						  		l2 += lights[k]->illuminateG(p, (Sphere*) objets[o], tem);
-						  		l3 += lights[k]->illuminateB(p, (Sphere*) objets[o], tem);
-						  	}
+                        for(int k = 0; k < lights.size(); ++k)
+                        {
+                            l1 += lights[k]->illuminateR(p, (Sphere*) objets[o], tem);
+                            l2 += lights[k]->illuminateG(p, (Sphere*) objets[o], tem);
+                            l3 += lights[k]->illuminateB(p, (Sphere*) objets[o], tem);
+                        }
 
-						  	l1 = std::min(l1, 255.);
-						  	l2 = std::min(l2, 255.);
-						  	l3 = std::min(l3, 255.);
+                        l1 = std::min(l1, 255.);
+                        l2 = std::min(l2, 255.);
+                        l3 = std::min(l3, 255.);
 
-						  	mL1 += l1;
-						  	mL2 += l2;
-						  	mL3 += l3;
+                        mL1 += l1;
+                        mL2 += l2;
+                        mL3 += l3;
 
-						}
+                    }
 
-				  }
-						mL1 /= 9.0;
-						mL2 /= 9.0;
-						mL3 /= 9.0;
-
-					    s.set_pixel(i, j, color((int)mL1, (int)mL2, (int)mL3));
-
-		        }
-        }
-    }
-
-    Scene::ThreadRender::ThreadRender(Scene& _sc,int _x, int _y, int _w, int _h, screen& _s) :
-        sc(_sc),x(_x), y(_y), w(_w), h(_h), s(_s)
-    {
-
-    }
-    void Scene::ThreadRender::run()
-    {
-        sc.renderArea(x,y,w,h,s);
-    }
-
-    void Scene::render(screen& s, int nbThreads)
-    {
-        std::cout << "Rendering with " << nbThreads <<  " threads." << std::endl;
-
-        std::cout << getNbObjects() << " objects. " << getNbLights() << " lights." << std::endl;
-        // Most of the times, the number of procs is a power of 2.
-        // Anyway, it's rarely a prime number (except 2...).
-        int p2 = std::log(nbThreads) / std::log(2);//More efficient to detect the most significant bit
-        int nb_w = 0; // Nombre de divisions de la largeur
-        int nb_h = 0; // Nombre de morceaux en hauteur
-        std::vector<ThreadRender*> threads(nbThreads);//A "pool" of threads
-        //What is the longest side ?
-        if(s.height() > s.width())
-        {
-            nb_w = std::pow(2, p2 / 2);
-            nb_h = nbThreads / nb_w ;
-        }
-        else
-        {
-            nb_h = std::pow(2, p2 / 2);
-            nb_w = nbThreads / nb_h ;
-        }
-
-        int w = s.width() / nb_w;
-        int h = s.height() / nb_h;
-        std::cout << "Parts are " << w << " x " << h << std::endl;
-        std::cout << "There are " << nb_w << " parts in width and " << nb_h << " in height." << std::endl;
-
-        time_t beginning = std::time(NULL);
-
-        //int limit_w = w * (nb_w - 1); // useful when there are parts left by the divisions above.
-        //int limit_h = h * (nb_h - 1 );
-        int k = 0;
-        //std::cout << "Number of simple parts : limit_w = " << limit_w << " and limit_h = " << limit_h << std::endl;
-        for(int i = 0 ; i <  s.width(); i += w)
-            for(int j = 0; j < s.height(); j += h)
+                }
+            if(oversampling)
             {
-                std::cout << "Rendering of " << i << "," << j << " - " << w << "," << h << std::endl;
-                threads[k] = new ThreadRender(*this,i,j,w,h,s);
-                //renderArea(i,j,w ,h,s);
-                k++;
+                mL1 /= 9.0;
+                mL2 /= 9.0;
+                mL3 /= 9.0;
             }
-        for(k=0; k < nbThreads ;k++)
-        {
-            std::cout << "Launching thread n." << k + 1<< std::endl;
-            threads[k]->exec();
+
+            s.set_pixel(i, j, color((int)mL1, (int)mL2, (int)mL3));
+
         }
-        s.update();
-        for(k=0;k < nbThreads; k++)
+    }
+}
+
+Scene::ThreadRender::ThreadRender(Scene& _sc,int _x, int _y, int _w, int _h, screen& _s) :
+    sc(_sc),x(_x), y(_y), w(_w), h(_h), s(_s)
+{
+
+}
+void Scene::ThreadRender::run()
+{
+    sc.renderArea(x,y,w,h,s);
+}
+
+void Scene::render(screen& s, int nbThreads)
+{
+    std::cout << "Rendering with " << nbThreads <<  " threads." << std::endl;
+
+    std::cout << getNbObjects() << " objects. " << getNbLights() << " lights." << std::endl;
+    // Most of the times, the number of procs is a power of 2.
+    // Anyway, it's rarely a prime number (except 2...).
+    int p2 = std::log(nbThreads) / std::log(2);//More efficient to detect the most significant bit
+    int nb_w = 0; // Nombre de divisions de la largeur
+    int nb_h = 0; // Nombre de morceaux en hauteur
+    std::vector<ThreadRender*> threads(nbThreads);//A "pool" of threads
+    //What is the longest side ?
+    if(s.height() > s.width())
+    {
+        nb_w = std::pow(2, p2 / 2);
+        nb_h = nbThreads / nb_w ;
+    }
+    else
+    {
+        nb_h = std::pow(2, p2 / 2);
+        nb_w = nbThreads / nb_h ;
+    }
+
+    int w = s.width() / nb_w;
+    int h = s.height() / nb_h;
+    std::cout << "Parts are " << w << " x " << h << std::endl;
+    std::cout << "There are " << nb_w << " parts in width and " << nb_h << " in height." << std::endl;
+
+    time_t beginning = std::time(NULL);
+
+    //int limit_w = w * (nb_w - 1); // useful when there are parts left by the divisions above.
+    //int limit_h = h * (nb_h - 1 );
+    int k = 0;
+    //std::cout << "Number of simple parts : limit_w = " << limit_w << " and limit_h = " << limit_h << std::endl;
+    for(int i = 0 ; i <  s.width(); i += w)
+        for(int j = 0; j < s.height(); j += h)
         {
-            threads[k]->join();
-            s.update();//Probably useless
+            std::cout << "Rendering of " << i << "," << j << " - " << w << "," << h << std::endl;
+            threads[k] = new ThreadRender(*this,i,j,w,h,s);
+            //renderArea(i,j,w ,h,s);
+            k++;
         }
-
-        for(k=0; k < nbThreads;k++)
-            delete threads[k];
-        //Render the last parts (w,h + reminder of the devision)
-        /*std::cout << "Rendering last parts" << std::endl;
-        for(unsigned i = 0; i < limit_w ; i += w)
-            renderArea(i, limit_h, w, s.height() - h, s);
-        for(unsigned j=0; j < limit_h; j += h)
-            renderArea(limit_w, j, s.width() - h, h, s);*/
-
-        time_t end = std::time(NULL);
-        std::cout << "End of rendering. Took " << end - beginning << " seconds." << std::endl;
- 	}
-
-    void Scene::render(screen& s)
+    for(k=0; k < nbThreads ; k++)
     {
-        render(s, Thread::nbCores());
+        std::cout << "Launching thread n." << k + 1<< std::endl;
+        threads[k]->exec();
+    }
+    s.update();
+    for(k=0; k < nbThreads; k++)
+    {
+        threads[k]->join();
+        s.update();//Probably useless
     }
 
-    void Scene::addMesh(Mesh* mesh)
-    {
-		objets.push_back(mesh);
-    }
+    for(k=0; k < nbThreads; k++)
+        delete threads[k];
+    //Render the last parts (w,h + reminder of the devision)
+    /*std::cout << "Rendering last parts" << std::endl;
+    for(unsigned i = 0; i < limit_w ; i += w)
+        renderArea(i, limit_h, w, s.height() - h, s);
+    for(unsigned j=0; j < limit_h; j += h)
+        renderArea(limit_w, j, s.width() - h, h, s);*/
 
-    void Scene::addLight(Light* light)
-    {
-		lights.push_back(light);
-    }
+    time_t end = std::time(NULL);
+    std::cout << "End of rendering. Took " << end - beginning << " seconds." << std::endl;
+}
 
-    void Scene::setCamera(Camera* camera)
-    {
-		cam = camera;
-    }
+void Scene::render(screen& s)
+{
+    render(s, Thread::nbCores());
+}
+
+void Scene::addMesh(Mesh* mesh)
+{
+    objets.push_back(mesh);
+}
+
+void Scene::addLight(Light* light)
+{
+    lights.push_back(light);
+}
+
+void Scene::setCamera(Camera* camera)
+{
+    cam = camera;
+}
 
 }
