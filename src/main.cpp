cef8057e490d09817024acdcdfd4915d52de6b31
programLyrique <program@lyrique.org>
2012-12-08 08:21:38 +0100

    Less warnings and adding a timing functionnality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <ctime>
 
 #include "screen.hpp"
 #include "scene.h"
@@ -7,22 +8,30 @@
 #include "texture.h"
 #include "pointlight.h"
 #include "ptScenes.h"
-#include <cmath>
-#include <sstream>
 
 
-int main(int argc, char** argv)
+
+int main()
 {
     rt::screen s(640,480);
 
 	// here the code to draw the pixels...
     Four_Spheres scene;
 
+    std::cout << "Starting rendering." << std::endl;
+
+    time_t beginning = std::time(NULL);
+
     scene.render(s);
+
+    time_t end = std::time(NULL);
+
+    std::cout << "End of rendering. Took : " << end - beginning << " seconds." << std::endl;
 
     while(s.update()) {
 
     }
+
 
     return 0;
 }
